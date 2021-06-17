ba9c4947953d10aa1d60ae6e04999a842c5f069f
TahaAttari <taha.attari.1@gmail.com>
2021-06-17 18:52:14 -0400

    basic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule, NativeDateModule, MAT_DATE_FORMATS } from '@angular/material/core';
+
+import { MillisecondFormatPipe } from './millisecond-format.pipe';
 
 import { AppComponent } from './app.component';
 import { ApiService } from './services/api-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MillisecondFormatPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    NativeDateModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    ApiService
+    ApiService,
   ],
   bootstrap: [AppComponent]
 })
